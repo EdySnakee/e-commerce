267de37595fe8df6fdee133753fdf2a7101d7a62
Edy <contactoedysnake@gmail.com>
2024-01-14 15:24:48 -0600

    [fire-base-conect]

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,14 @@ export const environment = {
   production: false,
   name: 'default',
   firebase: {
-
+    config : {
+      apiKey: "AIzaSyCccvDfuIgpIj8J_Uk-d6h26MocZqLYeV0",
+      authDomain: "e-commerce-c842b.firebaseapp.com",
+      projectId: "e-commerce-c842b",
+      storageBucket: "e-commerce-c842b.appspot.com",
+      messagingSenderId: "678163918203",
+      appId: "1:678163918203:web:5a156b6f98edddae6f61de"
+    }
   },
   actionCodeSettings: {
     url: 'http://localhost:5200/profile/new',
