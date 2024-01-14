8ab9ab1e44f3824bda9b04276d12afb19df001d2
Edy <contactoedysnake@gmail.com>
2024-01-14 14:32:30 -0600

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,14 @@ export const environment = {
   production: true,
   name: 'prod',
   firebase: {
-
+    config : {
+      apiKey: "AIzaSyCccvDfuIgpIj8J_Uk-d6h26MocZqLYeV0",
+      authDomain: "e-commerce-c842b.firebaseapp.com",
+      projectId: "e-commerce-c842b",
+      storageBucket: "e-commerce-c842b.appspot.com",
+      messagingSenderId: "678163918203",
+      appId: "1:678163918203:web:5a156b6f98edddae6f61de"
+    }
   },
   actionCodeSettings: {
     url: 'https://ecommerce-e4e6c.web.app/profile/new',
